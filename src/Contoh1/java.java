cc2b743afa18c9a510f1a62a7c93d240c08db46c
wonder woman <wonder woman@LAPTOP-JRCMS98G>
2018-08-02 11:14:09 +0700

    FOR WHILE DOWHILE

---
+++ src/Contoh1/java.java
@@ -0,0 +1,31 @@
+
+package Contoh1;
+
+public class java {
+    public static void main(String[] args) 
+    {
+        //Perulangan FOR
+        int x;
+        for(x = 1; x <= 5; x++)
+        {
+            System.out.println("SMK Telkom Malang");
+        }        
+      
+        //Perulangan WHILE
+        int i = 1;
+        while(i <= 5)
+        {
+            System.out.println("SMK Telkom Malang");
+            i++;
+        }
+        //Perulangan DO WHILE
+        int j = 1;
+        do
+        {
+            System.out.println("SMK Telkom Malang");
+            j++;
+        
+        }
+        while(j <= 5);     
+    }        
+}
